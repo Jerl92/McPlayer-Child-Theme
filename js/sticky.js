423c40295579254f77f3e8deb6039618fc17185a
Jeremie Langevin <jeremie.langevin@outlook.com>
2018-03-04 02:17:36 -0500

    Fix issues when the header is resized

--- js/sticky.js
+++ js/sticky.js
@@ -5,39 +5,44 @@ function stickIt_($) {
 	'use strict';
    
     // Create a clone of the site-header, right next to original.
-    $('.site-header').addClass('original').clone().insertAfter('.site-header').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
+    $('.site-header').addClass('original').clone().insertAfter('.site-header').addClass('cloned').css('position','sticky').css('margin-top','0').css('z-index','500').removeClass('original').hide();
 
     scrollIntervalID = setInterval(stickIt, 1);
-
 
     function stickIt() {
 
       var orgElementPos = $('.original').offset();
-      orgElementTop = 1;               
-      $wpAdminBar = $('#wpadminbar');
+      orgElementTop = 1;
+      $wpAdminBar = $("#wpadminbar");
       $windowwidth = $(window).width();
+      $wpAdminBarheight = $("#wpadminbar").height();
 
-      if ($windowwidth >= 450) {
+      if ($windowwidth >= 765) {
         if ($(window).scrollTop() >= (orgElementTop)) {
           // scrolled past the original position; now only show the cloned, sticky element.
 
-          // Cloned element should always have same left position and width as original element.     
+          // Cloned element should always have same left position and width as original element.   
           orgElement = $('.original');
           coordsOrgElement = orgElement.offset();
-          leftOrgElement = coordsOrgElement.left;  
+          leftOrgElement = coordsOrgElement.left;
           widthOrgElement = orgElement.css('width');
           if ($wpAdminBar.length) {
-            $('.cloned').css('left',leftOrgElement+'px').css('top',32).css('width',widthOrgElement).show();
-            $('.original').css('visibility','hidden');
+            $('.cloned').css('top', $wpAdminBarheight).css('width', '100%').show();
+            $('.original').hide();
           } else {
-            $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
-            $('.original').css('visibility','hidden');
+            $('.cloned').css('top', 0).css('width', '100%').show();
+            $('.original').hide();
           }
           $('.site-title').css('font-size','1.75rem');
         } else {
-          // not scrolled past the site-header; only show the original site-header.
-          $('.cloned').hide();
-          $('.original').css('visibility','visible');
+          if ($wpAdminBar.length) {
+            // not scrolled past the site-header; only show the original site-header.
+            $('.cloned').hide();
+            $('.original').css('top', $wpAdminBarheight).css('width', '100%').show();
+          } else {
+            $('.cloned').hide();
+            $('.original').css('top', 0).css('width', '100%').show();
+          }
           $('.site-title').css('font-size','2rem');
         }
       }
@@ -45,5 +50,9 @@ function stickIt_($) {
   }
 
   jQuery(document).ready(function($) {
+    stickIt_($);
+  });
+
+  jQuery(window).resize(function($) {
     stickIt_($);
   });

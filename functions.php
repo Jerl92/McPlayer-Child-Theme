f95100e3f552881c63185cb850fd386878803fce
Jeremie Langevin <jeremie.langevin@outlook.com>
2018-08-11 19:43:02 -0400

    fix double header

--- functions.php
+++ functions.php
@@ -1,14 +1,13 @@
 <?php 
-	add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-	function my_theme_enqueue_styles() { 
- 		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 	} 
-
-	 
 	add_action( 'after_setup_theme', 'mcplayer_support' );
 	function mcplayer_support() {
 		add_theme_support( 'music' );
 	}
+
+	add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+	function my_theme_enqueue_styles() { 
+ 		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
+ 	}
 	
 	add_action( 'wp_enqueue_scripts', 'my_chichi_navigation_enqueue' );
 	function my_chichi_navigation_enqueue() { 
--- js/sticky.js
+++ js/sticky.js
@@ -16,16 +16,18 @@ function stickIt_($) {
       $wpAdminBarheight = jQuery("#wpadminbar").height();
 
       if ($windowwidth >= 765) {
-        if (jQuery(window).scrollTop() > 0) {
+        if (jQuery(window).scrollTop() >= 1) {
           // scrolled past the original position; now only show the cloned, sticky element.
 
           // Cloned element should always have same left position and width as original element.   
           if ($wpAdminBar.length) {
             jQuery('.cloned').css('top', $wpAdminBarheight).css('width', '100%').show();
+            jQuery('.cloned').css('display', 'fixed');
             jQuery('.site-content').css('padding-top', '92px');
             jQuery('.original').hide();
           } else {
             jQuery('.cloned').css('top', 0).css('width', '100%').show();
+            jQuery('.cloned').css('display', 'fixed');
             jQuery('.site-content').css('padding-top', '92px');
             jQuery('.original').hide();
           }
@@ -34,10 +36,12 @@ function stickIt_($) {
           if ($wpAdminBar.length) {
             // not scrolled past the site-header; only show the original site-header.
             jQuery('.cloned').hide();
+            jQuery('.cloned').css('display', 'none');
             jQuery('.site-content').css('padding-top', 0);
             jQuery('.original').css('top', $wpAdminBarheight).css('width', '100%').show();
           } else {
             jQuery('.cloned').hide();
+            jQuery('.cloned').css('display', 'none');
             jQuery('.site-content').css('padding-top', 0);
             jQuery('.original').css('top', 0).css('width', '100%').show();
           }
@@ -59,16 +63,18 @@ function stickIt_($) {
     $wpAdminBarheight = jQuery("#wpadminbar").height();
 
     if ($windowwidth >= 765) {
-      if (jQuery(window).scrollTop() > 0) {
+      if (jQuery(window).scrollTop() >= 1) {
         // scrolled past the original position; now only show the cloned, sticky element.
 
         // Cloned element should always have same left position and width as original element.   
         if ($wpAdminBar.length) {
           jQuery('.cloned').css('top', $wpAdminBarheight).css('width', '100%').show();
+          jQuery('.cloned').css('display', 'fixed');
           jQuery('.site-content').css('padding-top', '92px');
           jQuery('.original').hide();
         } else {
           jQuery('.cloned').css('top', 0).css('width', '100%').show();
+          jQuery('.cloned').css('display', 'fixed');
           jQuery('.site-content').css('padding-top', '92px');
           jQuery('.original').hide();
         }
@@ -77,10 +83,12 @@ function stickIt_($) {
         if ($wpAdminBar.length) {
           // not scrolled past the site-header; only show the original site-header.
           jQuery('.cloned').hide();
+          jQuery('.cloned').css('display', 'none');
           jQuery('.site-content').css('padding-top', 0);
           jQuery('.original').css('top', $wpAdminBarheight).css('width', '100%').show();
         } else {
           jQuery('.cloned').hide();
+          jQuery('.cloned').css('display', 'none');
           jQuery('.site-content').css('padding-top', 0);
           jQuery('.original').css('top', 0).css('width', '100%').show();
         }
